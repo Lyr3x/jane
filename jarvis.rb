7037c6b26f0cdc398c1eeb62542c8c19ba3e71b9
Maik Schwan <nesurion@macrocker.fritz.box>
2014-10-02 15:45:11 +0200

    sinatra is entering the stage

---
+++ jarvis.rb
@@ -0,0 +1,32 @@
+#Jarvis Brain
+require 'sinatra'
+
+#listen to 0.0.0.0 instead of localhost
+set :bind, '0.0.0.0'
+
+get '/' do
+	erb :index
+end
+
+#Power commands
+#power on/off all devices
+get '/power' do
+	`ruby /Users/nesurion/Programming/JarvisServer/rightarm.rb`
+	'created test file'
+end
+
+#power on tv
+get '/tv_power' do
+
+end
+
+#power on avr
+get '/avr_power' do
+
+end
+
+#power on htpc
+get '/htpc_power' do
+
+end
+
