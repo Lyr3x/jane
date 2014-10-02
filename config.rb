5081d5f1b89b6724ac68420db267b15a3b727a60
Maik Schwan <nesurion@macrocker.fritz.box>
2014-10-02 16:25:31 +0200

    first ir send test

---
+++ config.rb
@@ -0,0 +1,4 @@
+css_dir = "css"
+sass_dir = "scss"                                  # unchanged
+images_dir = "images"                              # unchanged
+javascripts_dir = "js"                             # unchanged
