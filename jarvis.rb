d79079475b8f037ceed7252bb157a7ea0809a2fd
Kai <kai.bepperling@gmail.com>
2014-10-04 00:23:50 +0200

    Update jarvis.rb

--- jarvis.rb
+++ jarvis.rb
@@ -15,11 +15,6 @@ get '/power' do
   	`irsend SEND_ONCE avr KEY_POWER`
                 sleep(2)
    	`irsend SEND_ONCE tv KEY_POWER`
-=======
-	`irsend SEND_ONCE avr KEY_POWER`
-		sleep(2)
-	`irsend SEND_ONCE tv KEY_POWER`
->>>>>>> 299ab4385e2332df47c59f4939f6448c079e6bcb
 end
 
 #power on tv
@@ -34,9 +29,6 @@ end
 
 #power on htpc
 get '/htpc_power' do
-<<<<<<< HEAD
-=======
 
->>>>>>> 299ab4385e2332df47c59f4939f6448c079e6bcb
 end
 
