6c61efa2c46dad48768525693a2a9e1c420ab345
Maik Schwan <nesurion@MacRocker.local>
2014-10-04 13:59:13 +0200

    fixes merge issue

--- jarvis.rb
+++ jarvis.rb
@@ -11,15 +11,9 @@ end
 #Power commands
 #power on/off all devices
 get '/power' do
-<<<<<<< HEAD
 	`irsend SEND_ONCE avr KEY_POWER`
 		sleep(2)
 	`irsend SEND_ONCE tv KEY_POWER`
-=======
-  	`irsend SEND_ONCE avr KEY_POWER`
-                sleep(2)
-   	`irsend SEND_ONCE tv KEY_POWER`
->>>>>>> c02522407749abf2aa5b9a0b270b6e922ea30a92
 end
 
 #power on tv
@@ -34,10 +28,6 @@ end
 
 #power on htpc
 get '/htpc_power' do
-<<<<<<< HEAD
-	
-=======
 
->>>>>>> c02522407749abf2aa5b9a0b270b6e922ea30a92
 end
 
