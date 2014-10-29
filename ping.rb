32e8ac481f69a5672a11065198815e459274db0f
Kai Bepperling <Kai@Kais-Mac-Pro.fritz.box>
2014-10-29 19:28:32 +0100

    added todo comment

--- ping.rb
+++ ping.rb
@@ -5,7 +5,7 @@ include Net
 
 Ping::TCP.service_check = true
 
-hosts = ['192.168.2.109', '192.168.1.999']
+hosts = ['192.168.2.999', '192.168.1.999']
 threads = []
 ping_objects = []
 
@@ -13,13 +13,9 @@ hosts.each do |ip|
    ping_objects << Net::Ping::TCP.new(ip)
    threads << Thread.new(ip, ping_objects.last) do |ip, p|
      loop do
-     	if hosts[0] || hosts [1]
-     		puts 'execute command'
-     	end
+     	#Abfrage ob host true/false
        sleep 2
      end
    end
 end
 threads.each { |th| th.join }
-
-
