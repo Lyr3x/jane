20fee25877f8ed7396f5a3a64f8619da1f8c6eb7
Kai Bepperling <Kai@Kais-Mac-Pro.fritz.box>
2014-10-29 17:29:09 +0100

    ping test script

--- ping.rb
+++ ping.rb
@@ -1,6 +1,18 @@
 require 'rubygems'
 require 'net/ping'
 
-kai = Net::Ping::TVP.new('ip')
+include Net
+
+Ping::TCP.service_check = true
+
+kai = Net::Ping::TCP.new('192.168.2.109')
 repeat = 5
-if kai.ping?
+
+(1..repeat).each do
+
+	if kai.ping?
+	  puts "TCP ping successful"
+	else
+	  puts "TCP ping unsuccessful: " + kai.exception
+	end
+end
