35d2d4e4677299579e9dcefa3967566f7ffe14d0
Kai Bepperling <Kai@Kais-Mac-Pro.fritz.box>
2014-11-06 20:50:21 +0100

    changed sleep time and added a status variable

--- addons/ping.rb
+++ addons/ping.rb
@@ -9,20 +9,19 @@ Ping::TCP.service_check = true
 hosts = ['192.168.2.109', '192.168.2.5']
 threads = []
 ping_objects = []
-
+status = false
 hosts.each do |ip|
    ping_objects << Net::Ping::TCP.new(ip)
    threads << Thread.new(ip, ping_objects.last) do |ip, p|
      loop do
       if p.ping == true
-        puts "#{ip}"
-        # execute commands
+        status = true
         else
         # sleeptime if no host is available
         sleep 240
       end
-
-       sleep 2
+      #execute commands
+      sleep 180
      end
    end
 end
