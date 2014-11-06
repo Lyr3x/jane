50d45a4e4f2370f197ef7e9b753fe096c179ba2a
Kai Bepperling <Kai@Kais-Mac-Pro.fritz.box>
2014-11-06 20:54:36 +0100

    removed redundant code and one more sleep fix

--- addons/ping.rb
+++ addons/ping.rb
@@ -9,19 +9,19 @@ Ping::TCP.service_check = true
 hosts = ['192.168.2.109', '192.168.2.5']
 threads = []
 ping_objects = []
-status = false
+
 hosts.each do |ip|
    ping_objects << Net::Ping::TCP.new(ip)
    threads << Thread.new(ip, ping_objects.last) do |ip, p|
      loop do
       if p.ping == true
-        status = true
-        else
-        # sleeptime if no host is available
-        sleep 240
+        #execute commands here
+      else
+        # 5 min sleeptime if no host is available
+        sleep 300
       end
-      #execute commands
-      sleep 180
+      #10 min sleep when host is available
+      sleep 600
      end
    end
 end
