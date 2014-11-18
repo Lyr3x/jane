bbacf03bfc1267acd7283b7d332e448418881d62
Nesurion <maik.schwan@gmail.com>
2014-11-18 18:01:33 +0100

    testing

--- testing.rb
+++ testing.rb
@@ -1,3 +1,4 @@
+require 'net/http'
 require 'rufus-scheduler'
 require File.join(File.expand_path(File.dirname(__FILE__)), "addons", "sunset.rb")
 require 'yaml'
@@ -17,19 +18,24 @@ puts sunset.time
 # 		end
 # 	end
 # end
-light_on_command = ""
-jane_config.each do |category|
-	category[:buttons].each do |button|
-		if button[:name] == sunset_config[:light_button_name]
-			light_on_command = button[:command]
-			# file.puts "executed command"
-    	end
-  end
-end
+# light_on_command = ""
+
+# puts light_on_command
+# eval(light_on_command)
+# Net::HTTP.get(URI('http://192.168.2.123:4567/socket/lamp/off'))
 
 
 job = Rufus::Scheduler.new
 
-job.at '2014-11-18 17:35:00' do
-	eval(light_on_command)
+# job.at '2014-11-18 17:55:00' do
+job.every '3s' do
+	puts "job!"
+	jane_config.each do |category|
+	category[:buttons].each do |button|
+		if button[:name] == sunset_config[:light_button_name]
+			light_on_command = button[:command]
+			# file.puts "executed command"
+    end
+  end
+end
 end
