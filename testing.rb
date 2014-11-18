6b64634d21bbd5d99ef501d22752dd24603be774
Nesurion <maik.schwan@gmail.com>
2014-11-18 20:06:31 +0100

    latest test

--- testing.rb
+++ testing.rb
@@ -4,11 +4,11 @@ require File.join(File.expand_path(File.dirname(__FILE__)), "addons", "sunset.rb
 require 'yaml'
 require 'sinatra'
 
-sunset_config = YAML.load_file(File.join(File.expand_path(File.dirname(__FILE__)), 'config' ,'config_sunset.yml'))
-jane_config = YAML.load_file(File.join(File.expand_path(File.dirname(__FILE__)), 'config' ,'config.yml'))
+# sunset_config = YAML.load_file(File.join(File.expand_path(File.dirname(__FILE__)), 'config' ,'config_sunset.yml'))
+# jane_config = YAML.load_file(File.join(File.expand_path(File.dirname(__FILE__)), 'config' ,'config.yml'))
 
-sunset = Sunset.new(sunset_config[:city])
-puts sunset.time
+# sunset = Sunset.new(sunset_config[:city])
+# puts sunset.time
 
 # File.open("log", 'w') { |file| file.write("your text") }
 # jane_config.each do |category|
@@ -27,15 +27,9 @@ puts sunset.time
 
 job = Rufus::Scheduler.new
 
-# job.at '2014-11-18 17:55:00' do
-job.every '3s' do
-	puts "job!"
-	jane_config.each do |category|
-	category[:buttons].each do |button|
-		if button[:name] == sunset_config[:light_button_name]
-			light_on_command = button[:command]
-			# file.puts "executed command"
-    end
-  end
-end
+
+time = '2014-11-18 18:20:00'
+job.at time do
+		puts "sun"
+		time = '2014-11-18 18:30:00'
 end
