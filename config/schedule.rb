1d8a4b0716bd1e4fc864feccdc8b8235a7500c73
Nesurion <maik.schwan@gmail.com>
2014-12-09 16:46:22 +0100

    fixes require sunset

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,10 @@
 # by default whenever writes bash -l -c 'command...' to crontab
 # since we want to run a ruby script we'll write the command ourself
+require File.join(File.expand_path(File.dirname(__FILE__)), "addons", "sunset.rb")
 set :job_template, nil
 
-sunset = `ruby ../sunset.rb`
-every 1.day, :at => sunset do
+sunset = Sunset.new("Bonn")
+every 1.day, :at => sunset.time do
 	command "ruby" + File.join(File.expand_path(File.dirname(__FILE__)), "..", "addons", "lighton.rb")
 end
 
