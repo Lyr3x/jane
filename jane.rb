5ada22721728ab95d5533fd0cc3f040c29958931
Nesurion <maik.schwan@gmail.com>
2015-01-07 22:04:40 +0100

    nicer code based on rubocop suggestions

--- jane.rb
+++ jane.rb
@@ -1,64 +1,65 @@
-#Jane
+# Jane
 require 'sinatra'
 require 'rufus-scheduler'
-#require File.join(File.expand_path(File.dirname(__FILE__)), "addons", "sunset.rb")
-#require File.join(File.expand_path(File.dirname(__FILE__)), "addons", "timer.rb")
+# require File.join(File.expand_path(File.dirname(__FILE__)), "addons", "sunset.rb")
+# require File.join(File.expand_path(File.dirname(__FILE__)), "addons", "timer.rb")
 require 'yaml'
-require "net/http"
+require 'net/http'
 
-#listen to 0.0.0.0 instead of localhost
+# listen to 0.0.0.0 instead of localhost
 set :bind, '0.0.0.0'
 
 helpers do
   def render_button(btn_desc)
-  	html_renderd_button = "<button type=\"button\" 
-  		class=\"btn #{btn_desc[:btn_class]} btn-lg btn-block\" 
-  		onclick=\"jane("
-  			
-  	btn_desc[:fn_args].each do |fn_arg|
-  		html_renderd_button += "'#{fn_arg}',"
-  	end
-  	html_renderd_button = html_renderd_button[0..-2] + ")\">"
-  		
-  	html_renderd_button += "<span class=\"glyphicon glyphicon-#{btn_desc[:icon]}\"></span>
-  		#{btn_desc[:name]}</button>"
+    html_renderd_button = "<button type=\"button\"
+      class=\"btn #{btn_desc[:btn_class]} btn-lg btn-block\"
+    onclick=\"jane("
+    
+    btn_desc[:fn_args].each do |fn_arg|
+      html_renderd_button += '#{fn_arg},'
+    end
+    html_renderd_button = html_renderd_button[0..-2] + ")\">"
+     
+    html_renderd_button += '<span class=\"glyphicon glyphicon-#{btn_desc[:icon]}\"></span>
+     #{btn_desc[:name]}</button>'
   end
 
   def render_category(category_hash)
-  	html_renderd_category = 
-  	"<h3><span class=\"glyphicon glyphicon-#{category_hash[:icon]}\"></span>  #{category_hash[:title]}</h3>
-  	<div class=\"well\">\n"
-  	
-  	category_hash[:buttons].each do |button|
-  		html_renderd_category += render_button(button)
-  	end
-  	html_renderd_category += "</div>"
-  	# return html_renderd_category
+    html_renderd_category =
+    "<h3><span class=\"glyphicon glyphicon-#{category_hash[:icon]}\"></span>  #{category_hash[:title]}</h3>
+    <div class=\"well\">\n"
+    
+    category_hash[:buttons].each do |button|
+      html_renderd_category += render_button(button)
+    end
+    html_renderd_category += '</div>'
+    # return html_renderd_category
   end
 end
 
-#render index.erb
+# render index.erb
 get '/' do
-	erb :index
+  erb :index
 end
 
-config = YAML.load_file(File.join(File.expand_path(File.dirname(__FILE__)), 'config' ,'config.yml'))
+config = YAML.load_file(File.join(File.expand_path(File.dirname(__FILE__)),
+                        'config', 'config.yml'))
 
 config.each do |category|
-	category[:buttons].each do |button|
-		route = "/"
-		button[:fn_args].each do |arg|
-			route += arg + "/"
-		end
-		route = route[0..-2]
-		self.send(:get, route) do
-			eval(button[:command])
-		end
-	end
+  category[:buttons].each do |button|
+    route = '/'
+    button[:fn_args].each do |arg|
+      route += arg + '/'
+    end
+    route = route[0..-2]
+    self.send(:get, route) do
+      eval(button[:command])
+    end
+  end
 end
 
-#sunset inital cron entry
+# sunset inital cron entry
 `cd /home/jarvis/Jane/ && whenever --update-cron`
 
-#Timer
-#timer(Time.now + 1 * 60, "Lampe aus")
+# Timer
+# timer(Time.now + 1 * 60, "Lampe aus")
