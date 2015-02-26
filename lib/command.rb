c57e55a7aa87a8bdb579dbd8067c26971a3df445
Nesurion <maik.schwan@gmail.com>
2015-02-26 17:33:27 +0100

    powerpis ip is hardcoded for now

--- lib/command.rb
+++ lib/command.rb
@@ -2,11 +2,10 @@ require "net/http"
 
 module Command
   def self.powerpi(device, task, sleep_time)
-    ip = "192.168.2.25"
     if task == "on"
       task = 1
     end
-    Net::HTTP.get(URI("http://#{ip}/lib/powerpi.php?action=setsocket&socket=#{device}&status=#{task}"))
+    Net::HTTP.get(URI("http://0.0.0.0/lib/powerpi.php?action=setsocket&socket=#{device}&status=#{task}"))
     sleep(sleep_time.to_i)
   end
 
