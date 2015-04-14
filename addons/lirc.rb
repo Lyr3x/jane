396228bbac292ec370fe015f9f5fecef41f44cec
Nesurion <maik.schwan@gmail.com>
2015-04-14 17:54:20 +0200

    add radio.rb & lirc logic

--- addons/lirc.rb
+++ addons/lirc.rb
@@ -2,6 +2,6 @@
 
 module Lirc
   def self.run(command_parameter)
-    
+    system "irsend SEND_ONCE #{command_parameter[:device]} #{command_parameter[:task]}"
   end
 end
---
+++ addons/radio.rb
@@ -0,0 +1,8 @@
+# radio
+
+module Radio
+  def self.run(command_parameter)
+    # to do: add command
+    system
+  end
+end
