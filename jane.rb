f77acfe01fce329bfc9c3f0ba74f7453be7f2397
Nesurion <maik.schwan@gmail.com>
2015-04-14 22:31:18 +0200

    fix ui rendering

--- jane.rb
+++ jane.rb
@@ -33,6 +33,23 @@ set :environment, :production
 #     :default_ttl => 604800
 
 helpers do
+  def render_ui(config)
+    ui = ""
+    sorted_by_device = {}
+    # fill hash with device keys and empty arrays
+    config.each do |button|
+      sorted_by_device[button[:device]] = []
+    end
+    # write buttons based on device in array at key_device
+    config.each do |button|
+      sorted_by_device[button[:device]] << button
+    end
+    sorted_by_device.each do |device_name, buttons|
+      ui << render_device(device_name, buttons)
+    end
+    return ui
+  end
+
   def render_button(button)
     button_options = {
       class: "btn #{button[:btn_class]} btn-lg btn-block",
@@ -43,25 +60,25 @@ helpers do
     "<button #{button_options.map { |k, v| "#{k}=\"#{v}\"" }.join(' ')}>#{content}</button>"
   end
 
-  def render_category(category_hash)
-    html_renderd_category =
-    "<div class=\"col-md-4 col-sm-6\"><h3><span class=\"glyphicon glyphicon-#{category_hash[:icon]}\"></span>  #{category_hash[:title]}</h3>
-    <div class=\"well\">\n"
+  def render_device(device_name, buttons)
+    html_device =
+    "<div class=\"col-md-4 col-sm-6\"><h3>#{device_name}</h3><div class=\"well\">\n"
+    # <span class=\"glyphicon glyphicon-#{category_hash[:icon]}\"></span>  
 
-    category_hash[:buttons].each do |button|
-      html_renderd_category += render_button(button)
+    buttons.each do |button|
+      html_device += render_button(button)
     end
-    html_renderd_category += "</div></div>"
+    html_device += "</div></div>"
     
     @i += 1
     if @i%2==0
-      html_renderd_category += "<div class=\"clearfix visible-sm-block visible-md-block\"></div>" 
+      html_device += "<div class=\"clearfix visible-sm-block visible-md-block\"></div>" 
     end
 
     if @i%3==0
-      html_renderd_category += "<div class=\"clearfix visible-lg-block\"></div>" 
+      html_device += "<div class=\"clearfix visible-lg-block\"></div>" 
     end
-    return html_renderd_category
+    return html_device
   end
 end
 
