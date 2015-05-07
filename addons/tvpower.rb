ee19ebca036a744a212aca94f1d931534143c201
Nesurion <maik.schwan@gmail.com>
2015-05-07 20:27:10 +0200

    create hash for lirc module

--- addons/tvpower.rb
+++ addons/tvpower.rb
@@ -12,9 +12,10 @@ module Tvpower
     powerstatus = `/opt/vc/bin/tvservice -s`
     powerstatus = powerstatus.split[1]
     powerstatus = powerstatus[-1].to_i
+    lirc_command = {device: "tv", task: "KEY_POWER"}
     if powerstatus == 2 and command_parameter[:task] == "off" or \
       powerstatus == 1 and command_parameter[:task] == "on"
-      Lirc.run(command_parameter)
+      Lirc.run(lirc_command)
     end
     # if tv is on: state 0x40002 [NTSC 4:3], 720x480 @ 60.00Hz, interlaced
     # if tv is off: state 0x40001 [NTSC 4:3], 720x480 @ 60.00Hz, interlaced
