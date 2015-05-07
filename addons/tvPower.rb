51ffdf1a05f453f2cec48ad26ad19e5b2207aaaf
Nesurion <maik.schwan@gmail.com>
2015-05-07 20:02:18 +0200

    testable implementation

--- addons/tvPower.rb
+++ addons/tvPower.rb
@@ -1,19 +1,22 @@
 # tvPower
+lirc = File.expand_path(
+        File.join(
+          ENV['JANE_PATH'], 'addons', 'lirc'
+        )
+       )
+require lirc
+
 module TvPower
   def self.run(command_parameter)
     # get powerstatus from tvservice
     powerstatus = `/opt/vc/bin/tvservice -s`
     powerstatus = powerstatus.split[1]
-    p powerstatus
     powerstatus = powerstatus[-1].to_i
-    p powerstatus
-    if powerstatus == 1
-      return true
-    else
-      return false
+    if powerstatus == 2 and command_parameter[:task] == "off" or \
+      powerstatus == 1 and command_parameter[:task] == "on"
+      Lirc.run(command_parameter)
     end
-    # if tv is on: state 0x40001 [NTSC 4:3], 720x480 @ 60.00Hz, interlaced
-    # if tv is off: state 0x40002 [NTSC 4:3], 720x480 @ 60.00Hz, interlaced
-    # parse returned value to standby (true/false)
+    # if tv is on: state 0x40002 [NTSC 4:3], 720x480 @ 60.00Hz, interlaced
+    # if tv is off: state 0x40001 [NTSC 4:3], 720x480 @ 60.00Hz, interlaced
   end
 end
