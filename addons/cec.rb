c8018aa92ab8aa3b3577e8345bace4c8c2c0cd1d
Nesurion <maik.schwan@gmail.com>
2015-05-07 15:03:21 +0200

    cec.rb in alpha & example config update

--- addons/cec.rb
+++ addons/cec.rb
@@ -1,8 +1,10 @@
 # cec
 module Cec
   def self.run(command_parameter)
-    # get powerstatus from cec-client
-    system "echo pow 1 | cec-client -s -d 1"
+    # get powerstatus from tvservice
+    system "/opt/vc/bin/tvservice -s"
+    # if tv is on: state 0x12001a [HDMI CEA (16) RGB lim 16:9], 1920x1080 @ 60.00Hz, progressive
+    # if tv is off: state 0x120019 [HDMI CEA (16) RGB lim 16:9], 1920x1080 @ 60.00Hz, progressive
     # parse returned value to standby (true/false)
   end
 end
