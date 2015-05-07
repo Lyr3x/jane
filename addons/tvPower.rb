2ef2674738d04f20d30a14c88d1bf7b54709dbef
Nesurion <maik.schwan@gmail.com>
2015-05-07 19:44:07 +0200

    rename cec to tvPower & first implement + debug output

---
+++ addons/tvPower.rb
@@ -0,0 +1,19 @@
+# tvPower
+module TvPower
+  def self.run(command_parameter)
+    # get powerstatus from tvservice
+    powerstatus = `/opt/vc/bin/tvservice -s`
+    powerstatus = powerstatus.split[1]
+    p powerstatus
+    powerstatus = powerstatus[-1]
+    p powerstatus
+    if powerstatus == 1{
+      return true
+    } else {
+      return false
+    }
+    # if tv is on: state 0x40001 [NTSC 4:3], 720x480 @ 60.00Hz, interlaced
+    # if tv is off: state 0x40002 [NTSC 4:3], 720x480 @ 60.00Hz, interlaced
+    # parse returned value to standby (true/false)
+  end
+end
