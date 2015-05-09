8fc18eca352f8e63c18e3773f4b6f6eee54e99a5
Nesurion <maik.schwan@gmail.com>
2015-05-09 21:20:45 +0200

    adds radio.rb implementation #64

--- addons/radio.rb
+++ addons/radio.rb
@@ -1,8 +1,32 @@
 # radio
 
 module Radio
+  def self.get_config()
+    radio_config = File.expand_path(
+                     File.join(
+                       ENV['JANE_PATH'], 'config', 'radio.json'
+                     )
+                   )
+    JSON.parse(File.read(radio_config), symbolize_names: true)
+  end
+
   def self.run(command_parameter)
-    # to do: add command
-    system
+    code_hash = {"a" => 1, "b" => 2, "c" => 3, "d" => 4, "e" => 5, "f" => 6}
+    on_off_hash = {"off" => 0, "on" => 1}
+    radiosend = File.expand_path(
+                  File.join(
+                    ENV['JANE_PATH'], 'lib', 'radiosend'
+                  )
+                )
+    config = get_config
+    config.each do |remote|
+      remote[:devices].each do |device, code|
+        if device.to_s == command_parameter[:receiving_device]
+          system "#{radiosend} #{remote[:systemcode]} #{code_hash[code]} #{on_off_hash[command_parameter[:task]]}"
+          # puts "#{radiosend} #{remote[:systemcode]} #{code_hash[code]} #{on_off_hash[command_parameter[:task]]}"
+        else
+        end
+      end
+    end
   end
 end
