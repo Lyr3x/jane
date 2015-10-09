da73a9f8afffe5dafb8cc46c951de821e62961b7
Kai Bepperling <kai.bepperling@gmail.com>
2015-10-09 15:58:33 +0200

    removed unused config setup

--- addons/eiscp.rb
+++ addons/eiscp.rb
@@ -2,20 +2,21 @@ require 'rubygems'
 require 'json'
 
 module Eiscp
-  def self.config()
-    config_path = 
-      File.expand_path(
-        File.join(
-           ENV['JANE_PATH'], 'config', 'eiscp.json'
-        )
-      )
-    return JSON.parse(File.read(config_path), symbolize_names: true)
-  end
+  # def self.config()
+  #   config_path = 
+  #     File.expand_path(
+  #       File.join(
+  #          ENV['JANE_PATH'], 'config', 'eiscp.json'
+  #       )
+  #     )
+  #   return JSON.parse(File.read(config_path), symbolize_names: true)
+  # end
 
   def self.run(command_parameter)
 
-    host = config[:host]
-    `onkyo --host #{host} #{command_parameter[:command]}`
+    # host = config[:host]
+    # `onkyo --host #{host} #{command_parameter[:command]}`
+    `onkyo #{command_parameter[:command]}`
 
   end
 
