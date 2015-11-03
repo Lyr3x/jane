51560f2cca773adb7d907f01a2f8454cc01864f5
Nesurion <maik.schwan@gmail.com>
2015-11-03 17:25:39 +0100

    load right file for api calls

--- addons/sunset.rb
+++ addons/sunset.rb
@@ -7,7 +7,7 @@ ping = File.expand_path(
 
 apicall = File.expand_path(
         File.join(
-          ENV['JANE_PATH'], 'addons', 'ping'
+          ENV['JANE_PATH'], 'addons', 'apicall'
         )
        )
 
