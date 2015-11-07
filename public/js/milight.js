61d642040b150260a2fa9ead4c8a759675864fa6
Nesurion <maik.schwan@gmail.com>
2015-11-08 00:58:10 +0100

    adds all the milight features

--- public/js/milight.js
+++ public/js/milight.js
@@ -1,23 +1,44 @@
-$( document ).ready(function() {
-	var groupNr = 0;
+var groupNr = 0;
 
+$( document ).ready(function() {
 	var MilightRGB = function() {
 		$.post("http://192.168.2.101:8080/rgb" + "?group=" + groupNr + "&r=" + red.getValue() + "&g=" + green.getValue() + "&b=" + blue.getValue());
 	};
 	
 	var MilightBrightness = function() {
-		// $.post("http://192.168.2.101:8080/brightness", { group: groupNR, level: brightness })
 		$.post("http://192.168.2.101:8080/brightness" + "?group=" + groupNr + "&level=" + brightness.getValue());
+	};
+
+	var MilightOn = function() {
+		$.post("http://192.168.2.101:8080/on" + "?group=" + groupNr);
 	};
 	
 	var MilightOff = function() {
-		// $.post("http://192.168.2.101:8080/brightness", { group: groupNR, level: brightness })
 		$.post("http://192.168.2.101:8080/off" + "?group=" + groupNr);
 	};
-	
-	var MilightOn = function() {
-		// $.post("http://192.168.2.101:8080/brightness", { group: groupNR, level: brightness })
-		$.post("http://192.168.2.101:8080/on" + "?group=" + groupNr);
+
+	var MilightNight = function() {
+		$.post("http://192.168.2.101:8080/night" + "?group=" + groupNr);
+	};
+
+	var MilightWhite = function() {
+		$.post("http://192.168.2.101:8080/white" + "?group=" + groupNr);
+	};
+
+	var MilightDisco = function() {
+		$.post("http://192.168.2.101:8080/disco" + "?group=" + groupNr);
+	};
+
+	var MilightSpeedUp = function() {
+		$.post("http://192.168.2.101:8080/disco" + "?group=" + groupNr + "&speed=up");
+	};
+
+	var MilightSpeedDown = function() {
+		$.post("http://192.168.2.101:8080/disco" + "?group=" + groupNr + "&speed=down");
+	};
+
+	var MilightColor = function(color) {
+		$.post("http://192.168.2.101:8080/color" + "?group=" + groupNr + "&color=" + color);
 	};
 
 	var RGBChange = function() {
@@ -28,18 +49,55 @@ $( document ).ready(function() {
     $("#brightness").on("slide", function(slideEvt) {
 			$("#brightnessVal").text(slideEvt.value);
     });
-	
-	var red = $('#R').slider().on('slide', MilightRGB, RGBChange).data('slider');
 
+    var RGB = function(){
+    	MilightRGB();
+    	RGBChange();
+    };
 	
-	var green = $('#G').slider().on('slide', MilightRGB, RGBChange).data('slider');
+	var red = $('#R').slider().on('slide', RGB).data('slider');
 	
-	var blue = $('#B').slider().on('slide', MilightRGB, RGBChange).data('slider');
+	var green = $('#G').slider().on('slide', RGB).data('slider');
+	
+	var blue = $('#B').slider().on('slide', RGB).data('slider');
 	
 	var brightness = $('#brightness').slider().on('slide', MilightBrightness).data('slider');
-	
-	$('label[name=group]').click(function() {
+
+	$('input[name=group]').change(function() {
 		groupNr = $(this).attr("value");
-		alert("groupNr" + groupNr);
+	});
+
+	$('.color').click(function() {
+		color_raw = $(this).text().toLowerCase();
+		color = color_raw.replace(" ", "_");
+		MilightColor(color);
+	});
+
+	$('#on').click(function() {
+		MilightOn();
+	});
+
+	$('#off').click(function() {
+		MilightOff();
+	});
+
+	$('#night').click(function() {
+		MilightNight();
+	});
+
+	$('#white').click(function() {
+		MilightWhite();
+	});
+
+	$('#disco').click(function() {
+		MilightDisco();
+	});
+
+	$('#speedUp').click(function() {
+		MilightSpeedUp();
+	});
+
+	$('#speedDown').click(function() {
+		MilightSpeedDown();
 	});
 });
