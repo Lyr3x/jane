11e3659d2d660eef9b5c84a3b9282a9d49e6692e
Nesurion <maik.schwan@gmail.com>
2015-11-07 23:05:24 +0100

    javascript starting to work

--- public/js/milight.js
+++ public/js/milight.js
@@ -1,43 +1,39 @@
-var groupNr
+$( document ).ready(function() {
+	var groupNr
 
-var MilightRGB = function() {
-	$.post("http://192.168.2.101:8080/rgb" + "?group=" + groupNR + "&r=" + red + "&g=" + green + "&b=" + blue);
-};
+	var MilightRGB = function() {
+		$.post("http://192.168.2.101:8080/rgb" + "?group=" + groupNr + "&r=" + red.getValue() + "&g=" + green.getValue() + "&b=" + blue.getValue());
+	};
+	
+	var MilightBrightness = function() {
+		// $.post("http://192.168.2.101:8080/brightness", { group: groupNR, level: brightness })
+		$.post("http://192.168.2.101:8080/brightness" + "?group=" + groupNr + "&level=" + brightness.getValue());
+	};
+	
+	var MilightOff = function() {
+		// $.post("http://192.168.2.101:8080/brightness", { group: groupNR, level: brightness })
+		$.post("http://192.168.2.101:8080/off" + "?group=" + groupNr);
+	};
+	
+	var MilightOn = function() {
+		// $.post("http://192.168.2.101:8080/brightness", { group: groupNR, level: brightness })
+		$.post("http://192.168.2.101:8080/on" + "?group=" + groupNr);
+	};
 
-var MilightBrightness = function() {
-	// $.post("http://192.168.2.101:8080/brightness", { group: groupNR, level: brightness })
-	$.post("http://192.168.2.101:8080/brightness" + "?group=" + groupNR + "&level=" + brightness);
-};
-
-var MilightOff = function() {
-	// $.post("http://192.168.2.101:8080/brightness", { group: groupNR, level: brightness })
-	$.post("http://192.168.2.101:8080/off" + "?group=" + groupNR);
-};
-
-var MilightOn = function() {
-	// $.post("http://192.168.2.101:8080/brightness", { group: groupNR, level: brightness })
-	$.post("http://192.168.2.101:8080/on" + "?group=" + groupNR);
-};
-
-var red = $('#r').slider()
-				.on('slide', MilightRGB)
-				.data('slider');
-
-var green = $('#g').slider()
-					.on('slide', MilightRGB)
-					.data('slider');
-
-var blue = $('#b').slider()
-					.on('slide', MilightRGB)
-					.data('slider');
-
-var brightness = $('#brightness').slider()
-								.on('slide', MilightBrightness)
-								.data('slider');
-
-$(document).ready(function() {
-	$('.group').click(function() {
+	var RGBChange = function() {
+    	$('#RGB').css('background', 'rgb('+r.getValue()+','+g.getValue()+','+b.getValue()+')')
+    };
+	
+	var red = $('#R').slider().on('slide', MilightRGB).data('slider');
+	
+	var green = $('#G').slider().on('slide', MilightRGB).data('slider');
+	
+	var blue = $('#B').slider().on('slide', MilightRGB).data('slider');
+	
+	var brightness = $('#brightness').slider().on('slide', MilightBrightness).data('slider');
+	
+	$('label[name=group]').click(function() {
 		groupNr = $(this).attr("value");
+		alert("groupNr" + groupNr);
 	});
 });
-
